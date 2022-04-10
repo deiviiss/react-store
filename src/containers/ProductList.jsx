af1955ea71d04f56254436fab27ec5235fcba191
David Hilera <david.hilera@hotmail.com>
2022-04-10 07:46:13 -0500

    [UPDATE]Changed API

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ProductItem from "@components/ProductItem";
 import useGetProducts from "@hooks/useGetProducts";
-const API = "https://api.escuelajs.co/api/v1/products";
+const API = "https://init-store.herokuapp.com/api/v1/products";
 
 import "@styles/ProductList.scss";
 
